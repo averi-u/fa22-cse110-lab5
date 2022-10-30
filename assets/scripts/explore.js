f0d61591579cf78ffde4c78fec389be2f0b9bbe9
Averi Yu / Zhizhen Yu <73741014+averi-u@users.noreply.github.com>
2022-10-29 20:51:40 -0700

    upd

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,40 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const texts = document.getElementById("text-to-speak");
+const soundPlay = document.querySelector("button");
+const voiceSelect = document.getElementById("voice-select");
+
+function text2speech(event) {
+  
+  soundPlay.addEventListener('click', function() {
+    let textEntered = new SpeechSynthesisUtterance(texts.value);
+    speechSynthesis.speak(textEntered);
+  });
+}
+
+function populateVoiceList() {
+
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
 
 function init() {
   // TODO
+  texts.addEventListener('change', text2speech());
+  populateVoiceList();
+  voiceSelect.addEventListener('change', populateVoiceList());
 }
